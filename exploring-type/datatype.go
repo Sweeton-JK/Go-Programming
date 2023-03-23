3543e90c280deeb9975f43c7321bb4601d9569bd
Jeyanth Kallis Sweeton <75828092+Sweeton-JK@users.noreply.github.com>
2023-03-23 22:15:41 +0530

    Data Types And Type Specifiers

---
+++ exploring-type/datatype.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var y = 10
+var a = "Sweet"
+
+var dialogue string  =`James said,"He is good at golang"`
+
+func main() {
+	fmt.Printf("%T\n",y)
+	fmt.Printf("%T\n",a)
+	fmt.Println(dialogue)
+	fmt.Printf("%T\n",dialogue)
+}
